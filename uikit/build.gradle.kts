4dc84a776b86ea2d85eb587072e187af77e581b0
CharlotteUMR <p_jruixu@tencent.com>
2024-09-04 18:33:51 +0800

    feat: 1.2 修改发布配置

--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -37,9 +37,16 @@ dependencies {
     implementation(libs.androidx.appcompat)
 }
 
+val publishVersion: String by extra
+val publishGroupId: String by extra
+val publishArtifactId: String by extra
 publishing {
     publications {
         register<MavenPublication>("release") {
+            groupId = publishGroupId
+            artifactId = publishArtifactId
+            version = publishVersion
+
             afterEvaluate {
                 from(components["release"])
             }
